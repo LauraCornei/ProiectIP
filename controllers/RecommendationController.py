1326c2ae6191c891c5ed083e873f00db9ccffcd6
sebi <ciobanusebastian2000@yahoo.com>
2020-04-09 11:42:37 +0300

    Recomandarea 7

--- controllers/RecommendationController.py
+++ controllers/RecommendationController.py
@@ -1,7 +1,6 @@
-from services.recommendation import Food, Restaurant, FoodForRestaurant
+from services.recommendation import Food, Restaurant, FoodForRestaurant, RestaurantByFood
 from flask_classful import FlaskView, route
 from bson.json_util import dumps
-
 
 class RecommendationController(FlaskView):
     @route('restaurant/<customer_id>')
@@ -15,3 +14,6 @@ class RecommendationController(FlaskView):
     def food_for_restaurant(self, restaurant_id):
         return dumps(FoodForRestaurant.main(restaurant_id))
 
+    @route('restaurant_by_food/<customer_id>')
+    def restaurant_by_food(self, customer_id):
+        return dumps(RestaurantByFood.main(customer_id))
