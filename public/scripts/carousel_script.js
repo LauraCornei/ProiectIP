7256e0f4cb3a77347b4691876dd4f059496c10f9
AndreiAvram99 <53441456+AndreiAvram99@users.noreply.github.com>
2020-04-13 01:15:17 +0300

    carousel

--- public/scripts/carousel_script.js
+++ public/scripts/carousel_script.js
@@ -2,42 +2,42 @@ const data = [
     {
         id: 0,
         name: 'Restaurant Title1',
-        img:
+        image:
             'https://lh3.googleusercontent.com/VPWlDzu1I_v36IBv7d_dx4z5tnQZVHwCM5ZNOqriAGx13ZZlz0zTK7tMmgzLxjkPizewSA_mYUy8qcN8sg=w1080-h608-p-no-v0',
         description: 'Restaurant description',
     },
     {
         id: 1,
         name: 'Restaurant Title2',
-        img:
+        image:
             'https://cdn.homedit.com/wp-content/uploads/2016/06/Reds-True-Barbecue-building.jpg',
         description: 'Restaurant description',
     },
     {
         id: 2,
         name: 'Restaurant Title3',
-        img:
+        image:
             'https://res.cloudinary.com/culturemap-com/image/upload/ar_4:3,c_fill,g_faces:center,w_1200/v1548434789/photos/288737_original.jpg',
         description: 'Restaurant description',
     },
     {
         id: 3,
         name: 'Restaurant Title4',
-        img:
+        image:
             'https://media-cdn.tripadvisor.com/media/photo-s/11/af/e6/4a/hasienda-restaurant-outside.jpg',
         description: 'Restaurant description',
     },
     {
         id: 4,
         name: 'Restaurant Title5',
-        img:
+        image:
             'https://www.oradesibiu.ro/wp-content/uploads/2019/09/terasa-arini-hotel-parc_3.jpeg',
         description: 'Restaurant description',
     },
     {
         id: 5,
         name: 'Restaurant Title6',
-        img:
+        image:
             'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS4IT06ih0OBogC0Q4IIx-ethTIhhucncwvReLxQbsYbEaTSmt2&usqp=CAU',
         description: 'Restaurant description',
     },
