98377642957c567b4d4a9905001826083c6ba19f
alexcambose <alexcambose@yahoo.com>
2020-05-06 11:02:40 +0300

    Dummy reviews

--- models/Reviews.py
+++ models/Reviews.py
@@ -1,11 +1,17 @@
 import json
 import requests
+from models.dummyReviews import dummy_reviews
+
 
 class Reviews(object):
+    # @staticmethod
+    # def all(token):
+    #     url = 'http://159.65.247.164:3003/api/reviews'
+    #     headers = {"Authorization": "Bearer " + token}
+    #     reviewsCollection = json.loads(requests.get(url, headers=headers).text)
+    #     reviewsCollection = reviewsCollection['data']['reviews']
+    #     return reviewsCollection
     @staticmethod
-    def all(token):
-        url = 'http://159.65.247.164:3003/api/reviews'
-        headers = {"Authorization": "Bearer " + token}
-        reviewsCollection = json.loads(requests.get(url, headers=headers).text)
-        reviewsCollection = reviewsCollection['data']['reviews']
-        return reviewsCollection
+    def by_provider_id(provider_id, token):
+        return dummy_reviews[provider_id]
+
