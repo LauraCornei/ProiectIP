9bc4596120d4a0b595ddc133faa905964b45ebc9
ardeipopa <andrei.popa.sc@gmail.com>
2020-05-06 11:46:00 +0300

    de continuat integrarea de la recommend food for restaurant

--- models/Foods.py
+++ models/Foods.py
@@ -1,11 +1,25 @@
-from db import foodsCollection
+import json
+import requests
 
-from bson import ObjectId
 
 class Foods(object):
     @staticmethod
     def all(token):
-        return list(foodsCollection.find())
+        url = 'http://159.65.247.164:3002/api/courses'
+        headers = {"Authorization": "Bearer " + token}
+        coursesCollection = json.loads(requests.get(url, headers=headers).text)
+        coursesCollection =  coursesCollection['data']['menus']
+        allCourses = []
+        for menu in coursesCollection:
+            for restaurant_menu in coursesCollection[menu]:
+                for course in coursesCollection[menu][restaurant_menu]:
+                    print(course)
+        return  coursesCollection
 
+    @staticmethod
     def by_id(id, token):
-        return foodsCollection.find_one({"_id": ObjectId(id)})
+        url = 'http://159.65.247.164:3002/api/providers/' + id
+        headers = {"Authorization": "Bearer " + token}
+        restaurantsCollection = json.loads(requests.get(url, headers=headers).text)
+        restaurantsCollection = restaurantsCollection['data']['provider']
+        return restaurantsCollection
--- models/Restaurants.py
+++ models/Restaurants.py
@@ -13,8 +13,8 @@ class Restaurants(object):
 
     @staticmethod
     def by_id(id, token):
-        url = 'http://159.65.247.164:3002/api/users/' + id
+        url = 'http://159.65.247.164:3002/api/providers/' + id
         headers = {"Authorization": "Bearer " + token}
         restaurantsCollection = json.loads(requests.get(url, headers=headers).text)
-        restaurantsCollection = restaurantsCollection['data']['user']
+        restaurantsCollection = restaurantsCollection['data']['provider']
         return restaurantsCollection
