4e891abf37f41b734b3da448784a43bb0e3a8029
killagann <dariapopovici8@gmail.com>
2020-05-08 11:28:03 +0300

    res_by_food

--- services/recommendation/RestaurantByFood.py
+++ services/recommendation/RestaurantByFood.py
@@ -4,8 +4,18 @@ from models.Restaurants import Restaurants
 from models.Reviews import Reviews
 from models.Foods import Foods
 
+# http://127.0.0.1:5000/recommendations/restaurant_by_food/5eb16fdf4afbf654966cb68d
+# eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1ZTk4YTIzNzk3YWVhMTM5YzJiOTk1OTIiLCJpYXQiOjE1ODgyNTgxMTZ9.1GCgbuMH_Pi9kNtIR7QTqrH7EIcW86rbjU_Fe1HYSVk
+# Bearer Token(Authorization)
+
 
 def main(customer_id, token):
-    restaurants_id = filter_res(Customers.by_id(customer_id, token), Reviews.all(token), Foods.all())
-    restaurants_data = map(lambda e: Restaurants.by_id(e), restaurants_id)
-    return restaurants_data
+    print("was")
+    x = Customers.by_id(customer_id, token)
+    print(x)
+    if x is None:
+        return "This customer shiran!"
+
+    restaurants_id = filter_res(Customers.by_id(customer_id, token), Reviews.all(token), Foods.all(token), Restaurants.all(token))
+    # restaurants_data = map(lambda e: Restaurants.by_id(e, token), restaurants_id)
+    return restaurants_id  # restaurants_data
