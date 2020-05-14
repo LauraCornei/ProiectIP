1aff0356cb9cea1915a82e1555ca3affb996f834
ardeipopa <andrei.popa.sc@gmail.com>
2020-05-14 10:50:45 +0300

    first test

--- algorithms_tests/test_restaurant_food_recommendation.py
+++ algorithms_tests/test_restaurant_food_recommendation.py
@@ -1,2 +1,7 @@
 import unittest
 from algorithms.restaurant_food_recommendation import final
+from models.Orders import Orders
+
+class TestRestaurantFoodRecommendation(unittest.TestCase):
+    def test_recommendation(self):
+        self.assertRaises(ValueError, final, "12345", "abcdef", Orders.all('1234') )
