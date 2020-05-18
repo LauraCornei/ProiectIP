890a4126f8ca453fbd0adaba15e1693966f83adc
ardeipopa <andrei.popa.sc@gmail.com>
2020-05-18 10:51:57 +0300

    constante la populare de orders

--- misc/populateDatabaseNew.py
+++ misc/populateDatabaseNew.py
@@ -66,35 +66,28 @@ def create_providers_profile():
 
 def pick_course_from_restaurant(restaurant_id, token):
     restaurant = Restaurants.by_id(restaurant_id, token)
-    restaurant_courses = restaurant['details']['menu']['courses']
+    restaurant_courses = restaurant[Constants.DETAILS][Constants.MENU][Constants.COURSES]
     random_index = random.randint(0, len(restaurant_courses) - 1)
-    return restaurant_courses[random_index]['_id']
+    return restaurant_courses[random_index][Constants.ID]
 
 
 def add_to_cart(token, restaurant_id, s):
     course_id = pick_course_from_restaurant(restaurant_id, token)
-    url = 'http://159.65.247.164:3000/api/v1/cart/add-product/' + course_id
+    url = Constants.URL_ADD_PRODUCT + course_id
     headers = {"Authorization": "Bearer " + token}
-    print("folosesc tokenul: ", token)
-    print("adaug la cart ")
-    print("url-ul e ", url)
     x = s.get(url, headers=headers)
-    print(x.text)
-    url = "http://159.65.247.164:3000/api/v1/cart/session"
-    # x = s.get(url, headers=headers)
-    # print("cartul are in el: ", x.text)
 
 
 def get_provider_id_by_email(email, token):
     restaurants = Restaurants.all(token)
     for restaurant in restaurants:
-        if restaurant['email'] == email:
-            return restaurant['_id']
+        if restaurant[Constants.EMAIL] == email:
+            return restaurant[Constants.ID]
 
 
-def create_order_for_user(user_dict, login_response, s):
+def create_order_for_user(login_response, s):
     fake = Faker()
-    token = login_response["token"]
+    token = login_response[Constants.TOKEN]
     headers = {"Authorization": "Bearer " + token,
                "Content-type": "application/json"}
     while True:
@@ -102,58 +95,50 @@ def create_order_for_user(user_dict, login_response, s):
         if len(name) >= 2:
             break
     with open('resources/providers.json') as json_file:
-        providers = json.load(json_file)["providers"]
+        providers = json.load(json_file)[Constants.PROVIDERS]
         provider_index = random.randint(0, len(providers) - 1)
-        provider_email = providers[provider_index]['email']
+        provider_email = providers[provider_index][Constants.EMAIL]
         provider_id = get_provider_id_by_email(provider_email, token)
         nr_of_courses_in_order = random.randint(1, 3)
-        order = {"userId": login_response["user"]["_id"],
-                 "email": login_response["user"]["email"],
+        order = {Constants.USER_ID: login_response[Constants.USER][Constants.ID],
+                 Constants.EMAIL: login_response[Constants.USER][Constants.EMAIL],
                  "userFirstName": name[0],
                  "userLastName": name[1],
                  "phoneNumber": "0748973012",
                  "paymentMethod": "cash",
-                 "restaurantId": provider_id
+                 Constants.RESTAURANT_ID: provider_id
                  }
         for i in range(nr_of_courses_in_order):
-            add_to_cart(token, order['restaurantId'], s)
-        url = "http://159.65.247.164:3000/api/v1/cart/session"
+            add_to_cart(token, order[Constants.RESTAURANT_ID], s)
+        url = Constants.URL_CART_SESSION
         x = s.get(url, headers=headers)
         print("cartul are in el: ", x.text)
-        url = "http://159.65.247.164:3000/api/v1/cart"
+        url = Constants.URL_CART
         request_data = {
-            "userId": login_response["user"]["_id"]
+            Constants.USER_ID: login_response[Constants.USER][Constants.ID]
         }
         encoder = json.JSONEncoder()
-        print("url-ul e ", url)
-        print("headerurile sunt ", headers)
-        print("body ul e ", encoder.encode(request_data))
         x = s.post(url, headers=headers, data=encoder.encode(request_data))
-        print("in cart se afla la adaugare : ", x.text)
-        url = "http://159.65.247.164:3000/api/v1/orders"
-        print("encodarea de la order e ", encoder.encode(order))
+        url = Constants.URL_ORDERS
         x = s.post(url, headers=headers, data=encoder.encode(order))
-        print("am adaugat in orders, speram ca are items ", x.text)
 
 
 def create_orders():
     with open('resources/users.json') as json_file:
-        users = json.load(json_file)["users"]
-        login_url = 'http://159.65.247.164:3002/api/users/login'
+        users = json.load(json_file)[Constants.USERS]
+        login_url = Constants.URL_LOGIN
         encoder = json.JSONEncoder()
         for user in users:
             s = requests.Session()
-            print("sunt la userul ", user)
-            login_data = {"email": user["email"], "password": user["password"]}
+            login_data = {Constants.EMAIL: user[Constants.EMAIL], "password": user["password"]}
             headers = {"Content-type": "application/json"}
             login_data = encoder.encode(login_data)
             response = json.loads(s.post(login_url, data=login_data, headers=headers).text)
-            print("am primit response-ul: ", response)
-            if response["success"]:
+            if response[Constants.SUCCESS]:
                 nr_of_orders = random.randint(3, 5)
                 for i in range(nr_of_orders):
-                    print("creez orderul nr", i)
-                    create_order_for_user(user, response, s)
+                    create_order_for_user(response, s)
+
 
 class Restaurants(object):
     @staticmethod
@@ -173,6 +158,4 @@ class Restaurants(object):
         return restaurantsCollection
 
 
-
-print("am porit")
 create_orders()
